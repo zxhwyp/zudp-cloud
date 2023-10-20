569097e9acfa8764e617cd0556a97a621e875217
周小华 <623605336@qq.com>
2023-10-20 16:21:32 +0800

    fix：修改配置

--- zudp-modules/zudp-gen/src/main/java/com/zudp/gen/ZudpGenApplication.java
+++ zudp-modules/zudp-gen/src/main/java/com/zudp/gen/ZudpGenApplication.java
@@ -20,15 +20,6 @@ public class ZudpGenApplication
     public static void main(String[] args)
     {
         SpringApplication.run(ZudpGenApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功   ლ(´ڡ`ლ)ﾞ  \n");
     }
 }
